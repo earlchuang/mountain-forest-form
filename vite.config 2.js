75c997d43a10c934f6dc447362fa90e79e7446f8
earlchuang <earlchuang@gmail.com>
2026-05-10 23:43:28 +0900

    Create vite.config 2.js

---
+++ vite.config 2.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+<<<<<<< HEAD
+})
+=======
+})
+>>>>>>> 8a5544ede6b2774117df0a8d4c207529f34604d7
